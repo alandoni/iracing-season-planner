4e8e97d2f1c62632b8515a49ac95520a75081dd1
Alan <alan.etm@gmail.com>
2024-09-05 15:28:56 -0300

    Changing import/export data title

--- frontend/src/view/components/header.tsx
+++ frontend/src/view/components/header.tsx
@@ -50,10 +50,10 @@ export function Header() {
           iRacing Season Planner
         </Text>
         <div className="header-buttons">
-          <span title="Import data">
+          <span title="Importar dados - Salvar em arquivo no seu computador">
             <Import className="svg icon import" viewBox="0 0 24 24" onClick={onImportClick} />
           </span>
-          <span title="Export data">
+          <span title="Exportar dados - Ler de um arquivo no seu computador">
             <Export className="svg icon export" viewBox="0 0 24 24" onClick={onExportClick} />
           </span>
         </div>
