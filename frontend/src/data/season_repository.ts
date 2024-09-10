3ecdc3d0322d8c0d2874c767d16902598a52dbb2
Alan <alan.etm@gmail.com>
2024-09-10 18:30:24 -0300

    Fix error when storing series

--- frontend/src/data/season_repository.ts
+++ frontend/src/data/season_repository.ts
@@ -22,25 +22,32 @@ export function useSeasonRepository() {
       return
     }
     const cachedDate = new Date(localStorage.getItem(LOCAL_STORAGE_CACHED_DATE_KEY) ?? "")
-    const seasonData = {
-      cars: JSON.parse(localStorage.getItem(LOCAL_STORAGE_CARS_KEY) ?? "[]"),
-      tracks: JSON.parse(localStorage.getItem(LOCAL_STORAGE_TRACKS_KEY) ?? "[]"),
-      series: JSON.parse(localStorage.getItem(LOCAL_STORAGE_SERIES_KEY) ?? "[]").map((s: Series) => ({
-        ...s,
-        schedules: s.schedules.map((sc: Schedule) => ({ ...sc, startDate: new Date(sc.startDate) })),
-      })),
-      licenses: JSON.parse(localStorage.getItem(LOCAL_STORAGE_LICENSES_KEY) ?? "[]"),
-      categories: JSON.parse(localStorage.getItem(LOCAL_STORAGE_CATEGORIES_KEY) ?? "[]"),
-      cachedDate: cachedDate,
-    }
-    const cachedSeason = Object.assign(new Season(), seasonData)
-    if (cachedSeason.validate()) {
-      console.log(`Using cache, it will expire on ${cachedDate}`)
-      setSeason(cachedSeason)
-    } else {
-      console.log("Making request")
-      makeRequest()
-    }
+
+    decompress(localStorage.getItem(LOCAL_STORAGE_SERIES_KEY) ?? "")
+      .then((data) => {
+        const seasonData = {
+          cars: JSON.parse(localStorage.getItem(LOCAL_STORAGE_CARS_KEY) ?? "[]"),
+          tracks: JSON.parse(localStorage.getItem(LOCAL_STORAGE_TRACKS_KEY) ?? "[]"),
+          series: JSON.parse(data ?? "[]").map((s: Series) => ({
+            ...s,
+            schedules: s.schedules.map((sc: Schedule) => ({ ...sc, startDate: new Date(sc.startDate) })),
+          })),
+          licenses: JSON.parse(localStorage.getItem(LOCAL_STORAGE_LICENSES_KEY) ?? "[]"),
+          categories: JSON.parse(localStorage.getItem(LOCAL_STORAGE_CATEGORIES_KEY) ?? "[]"),
+          cachedDate: cachedDate,
+        }
+        const cachedSeason = Object.assign(new Season(), seasonData)
+        if (cachedSeason.validate()) {
+          console.log(`Using cache, it will expire on ${cachedDate}`)
+          setSeason(cachedSeason)
+        } else {
+          console.log("Making request")
+          makeRequest()
+        }
+      })
+      .catch(() => {
+        makeRequest()
+      })
   }, [])
 
   useEffect(() => {
@@ -49,9 +56,13 @@ export function useSeasonRepository() {
     }
     if (data && data.cachedDate) {
       console.log(`Data not found, requested: ${data}`)
+
+      compress(JSON.stringify(data.series)).then((buffer) => {
+        localStorage.setItem(LOCAL_STORAGE_SERIES_KEY, buffer)
+      })
+
       localStorage.setItem(LOCAL_STORAGE_CARS_KEY, JSON.stringify(data.cars))
       localStorage.setItem(LOCAL_STORAGE_TRACKS_KEY, JSON.stringify(data.tracks))
-      localStorage.setItem(LOCAL_STORAGE_SERIES_KEY, JSON.stringify(data.series))
       localStorage.setItem(LOCAL_STORAGE_LICENSES_KEY, JSON.stringify(data.licenses))
       localStorage.setItem(LOCAL_STORAGE_CATEGORIES_KEY, JSON.stringify(data.categories))
       localStorage.setItem(LOCAL_STORAGE_CACHED_DATE_KEY, String(data.cachedDate) ?? "")
@@ -79,6 +90,67 @@ export function useSeasonRepository() {
     localStorage.removeItem(LOCAL_STORAGE_CATEGORIES_KEY)
     localStorage.removeItem(LOCAL_STORAGE_CACHED_DATE_KEY)
     makeRequest()
+  }
+
+  const compress = async (string: string): Promise<string> => {
+    const byteArray = new TextEncoder().encode(string)
+    const cs = new CompressionStream("gzip")
+    const writer = cs.writable.getWriter()
+    writer.write(byteArray)
+    writer.close()
+    const reader = cs.readable.getReader()
+    let totalSize = 0
+    //go through each chunk and add it to the output
+    let finished = true
+    const output: Uint8Array[] = []
+    do {
+      const { value, done } = await reader.read()
+      if (done) break
+      finished = done
+      output.push(value)
+      totalSize += value.byteLength
+    } while (!finished)
+
+    const concatenated = new Uint8Array(totalSize)
+    let offset = 0
+    //finally build the compressed array and return it
+    for (const array of output) {
+      concatenated.set(array, offset)
+      offset += array.byteLength
+    }
+    return concatenated.join()
+  }
+
+  const decompress = async (byteArrayInBase64: string): Promise<string> => {
+    if (byteArrayInBase64.length === 0) {
+      throw new Error("Invalid string")
+    }
+    const cs = new DecompressionStream("gzip")
+    const writer = cs.writable.getWriter()
+    writer.write(byteArrayInBase64)
+    writer.close()
+    //create the reader
+    const reader = cs.readable.getReader()
+    let totalSize = 0
+    //go through each chunk and add it to the output
+    let finished = true
+    const output: Uint8Array[] = []
+    do {
+      const { value, done } = await reader.read()
+      if (done) break
+      finished = done
+      output.push(value)
+      totalSize += value.byteLength
+    } while (!finished)
+
+    const concatenated = new Uint8Array(totalSize)
+    let offset = 0
+    //finally build the compressed array and return it
+    for (const array of output) {
+      concatenated.set(array, offset)
+      offset += array.byteLength
+    }
+    return concatenated.join()
   }
 
   return { data: season, loading, error, success, invalidateCache }
